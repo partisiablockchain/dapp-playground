3c6b3f949e6961e55a3958fb5161d0bf1107e550
Emil Drewsen Jørgensen <emil.drewsen.joergensen@secata.com>
2024-03-08 11:55:28 +0100

    Removed "cli", replaces "tx" with "transaction", updated rust version and npm package versions.

--- scripts/key-gen.sh
+++ scripts/key-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo partisia-contract cli account create --file Account-A.pk
-cargo partisia-contract cli account create --file Account-B.pk
-cargo partisia-contract cli account create --file Account-C.pk
+cargo partisia-contract account create --file Account-A.pk
+cargo partisia-contract account create --file Account-B.pk
+cargo partisia-contract account create --file Account-C.pk
